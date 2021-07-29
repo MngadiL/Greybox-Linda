e3ac375f59c4dfebb20e547a52cf827ad9189d85
MngadiL <grey_box_tech@yahoo.com>
2021-07-29 23:15:21 +0200

    slight changes

--- javascri.js
+++ javascri.js
@@ -1,9 +1,2 @@
-var buton = document.getElementById('proj_btn');
-buton.addEventListener("click", function (){
-    alert('Welcome to Projects');
-});
 
-var pics = document.getElementById('chart1');
-pics.addEventListener("click", function(){
-    resizeTo(1000,600);
-})
+
